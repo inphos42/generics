e482b10a03698281bb0b3a382d8591009185399a
Oliver Groß <neokensuke@freenet.de>
2013-02-10 12:50:02 +0100

    [store] added generic ID parameter and forward declaration

--- store.h
+++ store.h
@@ -1,19 +1,18 @@
 #ifndef GENERICS_STORE_H
 #define GENERICS_STORE_H
 
+#include "store_fwd.h"
+
 #include <unordered_map>
 #include <map>
 #include <deque>
 #include <string>
-#include <cstdint>
 
 namespace generics {
 
-	template< typename T >
+	template< typename T, typename ID >
 	class Store {
 	public:
-		typedef uint32_t ID;
-
 		struct StoreEntry {
 			T value;
 			int references;
@@ -44,34 +43,34 @@ namespace generics {
 		inline const_iterator cbegin() const { return m_Entries.cbegin(); }
 		inline const_iterator cend() const { return m_Entries.cend(); }
 
-		inline const T & query(uint32_t id) const { return m_Entries.at(id)->value; }
-		inline const T & operator[](uint32_t id) const { return query(id); }
+		inline const T & query(ID id) const { return m_Entries.at(id)->value; }
+		inline const T & operator[](ID id) const { return query(id); }
 
 		inline std::size_t size() const { return m_Entries.size(); }
 
-		inline uint32_t maxId() const { return m_IdCounter; }
+		inline ID maxId() const { return m_IdCounter; }
 
 	protected:
-		typedef typename std::unordered_map< uint32_t, StoreEntry * >::iterator iterator;
+		typedef typename std::unordered_map< ID, StoreEntry * >::iterator iterator;
 
-		typedef typename std::map< T, uint32_t >::const_iterator id_const_iterator;
-		typedef typename std::map< T, uint32_t >::iterator id_iterator;
+		typedef typename std::map< T, ID >::const_iterator id_const_iterator;
+		typedef typename std::map< T, ID >::iterator id_iterator;
 
-		std::unordered_map< uint32_t, StoreEntry * > m_Entries;
-		std::map< T, uint32_t > m_IdMap;
-		std::deque< uint32_t > m_FreeIds;
+		std::unordered_map< ID, StoreEntry * > m_Entries;
+		std::map< T, ID > m_IdMap;
+		std::deque< ID > m_FreeIds;
 
-		uint32_t m_IdCounter;
+		ID m_IdCounter;
 
 	private:
 		Store(const Store & other);
 		Store & operator=(const Store & other);
 	};
 
-	template< typename T >
-	uint32_t Store< T >::insert(const T & value)  {
+	template< typename T, typename ID >
+	ID Store< T, ID >::insert(const T & value)  {
 		id_const_iterator target = m_IdMap.find(value);
-		uint32_t result;
+		ID result;
 		StoreEntry * entry;
 
 		if (target == m_IdMap.end()) {
@@ -86,8 +85,8 @@ namespace generics {
 
 			entry = new StoreEntry(value, 0);
 
-			m_Entries.insert(std::pair< uint32_t, StoreEntry * >(result, entry));
-			m_IdMap.insert(std::pair< T, uint32_t >(value, m_Entries.size()));
+			m_Entries.insert(std::pair< ID, StoreEntry * >(result, entry));
+			m_IdMap.insert(std::pair< T, ID >(value, m_Entries.size()));
 		}
 		else {
 			result = target->second;
@@ -99,8 +98,8 @@ namespace generics {
 		return result;
 	}
 
-	template< typename T >
-	void Store< T >::remove(uint32_t id) {
+	template< typename T, typename ID >
+	void Store< T, ID >::remove(ID id) {
 		iterator entryIt = m_Entries.find(id);
 		if (entryIt == m_Entries.end())
 			return;
@@ -117,8 +116,8 @@ namespace generics {
 		}
 	}
 
-	template< typename T >
-	void Store< T >::remove(const T & value) {
+	template< typename T, typename ID >
+	void Store< T, ID >::remove(const T & value) {
 		id_iterator target = m_IdMap.find(value);
 		if (target == m_IdMap.end())
 			return;
@@ -136,8 +135,8 @@ namespace generics {
 		}
 	}
 
-	template< typename T >
-	void Store< T >::remove(StoreEntry * entry) {
+	template< typename T, typename ID >
+	void Store< T, ID >::remove(StoreEntry * entry) {
 		id_iterator target = m_IdMap.find(entry->value);
 		if (target == m_IdMap.end())
 			return;
@@ -150,8 +149,8 @@ namespace generics {
 		delete entry;
 	}
 
-	template< typename T >
-	void Store< T >::clear() {
+	template< typename T, typename ID >
+	void Store< T, ID >::clear() {
 		const_iterator stringIt = m_Entries.cbegin();
 		while (stringIt != m_Entries.cend()) {
 			delete stringIt->second;
---
+++ store_fwd.h
@@ -0,0 +1,10 @@
+#ifndef GENERICS_STORE_FDW_H
+#define GENERICS_STORE_FDW_H
+
+#include <cstdint>
+
+namespace generics {
+	template< typename T, typename ID = uint32_t> class Store;
+}
+
+#endif
