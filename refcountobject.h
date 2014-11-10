f628394565cffe6a81c29172bc2b93091f37fdc5
Daniel Bahrdt <daniel.git@funroll-loops.de>
2014-11-10 15:05:48 +0100

    refcounting: add RCPtr like std::shared_ptr (but not thread-safe)

--- refcountobject.h
+++ refcountobject.h
@@ -46,9 +46,59 @@ namespace generics {
 		bool operator!=(const RCWrapper & other) { return m_Private != other.m_Private; }
 
 		inline bool isNull() const { return !m_Private; }
+	protected:
+		void reset(RCClass * data) {
+			if (data) {
+				data->rcInc();
+			}
+			if (m_Private) {
+				m_Private->rcDec();
+			}
+			m_Private = data;
+		}
+		
+		inline RCClass * priv() const { return m_Private; }
 
 	protected:
 		RCClass * m_Private;
+	};
+	
+	template< class RCClass >
+	class RCPtr: RCWrapper<RCClass> {
+		void safe_bool_func() {}
+		typedef void (RCPtr<RCClass>:: * safe_bool_type) ();
+	public:
+		RCPtr() : RCWrapper<RCClass>() {};
+		explicit RCPtr(RCClass * data) : RCWrapper<RCClass>(data) {}
+		RCPtr(const RCWrapper<RCClass> & other) : RCWrapper<RCClass>(other) {}
+		virtual ~RCPtr() {}
+
+		RCPtr & operator=(const RCPtr& other) {
+			RCWrapper<RCClass>::operator=(other);
+			return *this;
+		}
+		
+		inline bool operator==(const RCPtr<RCClass> & other) { return RCWrapper<RCClass>::operator==(other); }
+		inline bool operator!=(const RCPtr<RCClass> & other) { return RCWrapper<RCClass>::operator!=(other); }
+		
+		RCClass & operator*() { return *RCWrapper<RCClass>::priv();}
+		const RCClass & operator*() const { return *RCWrapper<RCClass>::priv();}
+
+		RCClass * operator->() { return RCWrapper<RCClass>::priv();}
+		const RCClass * operator->() const { return RCWrapper<RCClass>::priv();}
+
+		RCClass * priv() { return RCWrapper<RCClass>::priv(); }
+		const RCClass * priv() const { return RCWrapper<RCClass>::priv(); }
+		
+		RCClass * get() { return priv(); }
+		const RCClass * get() const { return priv(); }
+		
+		operator safe_bool_type() const {
+			return priv() ? &RCPtr<RCClass>::safe_bool_func : 0;
+		}
+		void reset(RCClass * data) {
+			RCWrapper<RCClass>::reset(data);
+		}
 	};
 }
 
